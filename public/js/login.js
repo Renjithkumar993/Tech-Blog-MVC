a92df724051d6c64623de189e0347bc5861f9c2c
Renjithkumar993 <121647888+Renjithkumar993@users.noreply.github.com>
2023-04-23 22:39:51 -0300

    changed / to home

--- public/js/login.js
+++ public/js/login.js
@@ -14,7 +14,7 @@ const loginFormHandler = async (event) => {
       headers: { 'Content-Type': 'application/json' },
     });
   if (response.ok) {
-      document.location.replace("/");
+      document.location.replace("/home");
     } else {
       const responseData = await response.json();
       const errors = responseData.message
